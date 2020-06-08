92c360134a22dec9e00e07ac00843ae2ae6f731f
ijgnd <fooijgnd@barijgnd>
2020-06-08 02:40:22 +0200

    fix import old configs.

--- src/adjust_config.py
+++ src/adjust_config.py
@@ -184,6 +184,8 @@ def uses_most_recent_config(config, level):
 
 def update_config_for_202005(config):
     for e in config['v3']:
+        if not "Text_in_menu_styling" in e:
+            e["Text_in_menu_styling"] = ""    
         if not "Text_in_menu_styling_nightmode" in e:
             e["Text_in_menu_styling_nightmode"] = ""
     if not "v3_inactive" in config:
