61fbedf4943a85cf2c9d1215fe512c25df05fb67
Henrik Giesel <hengiesel@gmail.com>
2020-09-18 18:49:57 +0200

    Fix zip script work

--- bin/zip.sh
+++ bin/zip.sh
@@ -1,9 +1,9 @@
 declare DIR="$(cd "$(dirname "$0")/.." && pwd -P)"
-declare addon_id='persitent_editor'
-
-cd "$DIR"
+declare addon_id='custom_styles'
 
 "$DIR/bin/compile.sh"
 
+cd "$DIR/src"
+
 zip -r "$DIR/build/$addon_id.ankiaddon" \
-  "src/"*
+  *
