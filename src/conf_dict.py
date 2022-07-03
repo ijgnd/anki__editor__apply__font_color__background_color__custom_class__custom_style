303ab915238075eee1ecf56bd472077a5cefdc8a
ijgnd <does_not_exist@example.com>
2022-07-03 22:24:05 +0200

    remove unused start warning/message

--- src/conf_dict.py
+++ src/conf_dict.py
@@ -25,42 +25,6 @@ from .utils import (
 )
 
 
-first_start = f"""
-This is an infobox from the add-on "{addon_name}". It's shown one time because you either 
-installed it for the first time or just upgraded.
-The most recent version of this add-on contains a better default config. If you don't use a 
-custom config you should consider resetting the config of this add-on to it's default so that 
-you get the new config: It's in the upper right corner of the config dialog behind the "more" button. 
-DOUBLE
-This add-on has some quirks/limitations for technical reasons: You either know some 
-background about the anki editor and how to work around these or you will run into some problems 
-like disappearing markup or not being able to clear the formatting. If you can't live with 
-these limitations don't use this add-on. Just uninstall it. For details see the description 
-on ankiweb, {ankiwebpage}.
-DOUBLE
-This add-on only works if "@import url("_editor_button_styles.css");" is ontop (!) 
-the styling section of all your note types. I (addon-creator) have had many "bug" reports because 
-people didn't do this. So this add-on offers to automatically add this line to all your note types.
-DOUBLE
-If something went wrong there would be a lot of damage so that most likely only a backup would help. 
-The code that automatically updates your templates has been downloaded thousands of times and I 
-haven't heard a complaint. But better safe than sorry: So make sure to have backups and know 
-how to restore them. Read the section "Setup" on ankiweb, {addon_webpage}.
-DOUBLE
-If you don't update the templates now this add-on offers to auto-update your note types whenever 
-you change the add-on config.
-DOUBLE
-DOUBLE
-I have read the description on ankiweb and confirm to have a backup that I know how to restore.
-DOUBLE
-I want to auto-adjust the styling section of my note types if necessary now.
-""".replace(
-    "\n", ""
-).replace(
-    "DOUBLE", "\n\n"
-)
-
-
 def load_conf_dict():
     config = default_config.copy()
     if os.path.isfile(pickle_file):
