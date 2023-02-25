4e9709b6346aaf880353e69b371d8fd385f2a408
ijgnd <40218852+ijgnd@users.noreply.github.com>
2023-02-25 14:21:44 +0100

    avoid error messages on pyqt5

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,28 @@
-from aqt import mw
-
-from .config_var import init_config_var
-from .conf_dict import init_conf_dict
-from .main_window import init_main_window
-from .editor import init_editor
+from aqt.qt import qtmajor
+from aqt.utils import tooltip
+from .vars import addon_name
 
 
-regex = r"(web[/\\].*)"
-mw.addonManager.setWebExports(__name__, regex)
+if qtmajor == 5:
+    msg = f"""The addon "{addon_name}" does not support Anki versions built with pyqt5."""
+    print(msg)
+    tooltip(msg)
+else:
+    from aqt import mw
 
 
-# config: on startup load it, then maybe update old version, save on exit
-init_config_var()
-init_conf_dict()
-init_main_window()
-init_editor()
+    from .config_var import init_config_var
+    from .conf_dict import init_conf_dict
+    from .main_window import init_main_window
+    from .editor import init_editor
+
+
+    regex = r"(web[/\\].*)"
+    mw.addonManager.setWebExports(__name__, regex)
+
+
+    # config: on startup load it, then maybe update old version, save on exit
+    init_config_var()
+    init_conf_dict()
+    init_main_window()
+    init_editor()
